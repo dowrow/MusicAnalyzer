a1c86bcf33bd3dbdc2fa2f9ac646146297673449
Dowrow <dowrow@outlook.com>
2014-09-07 17:50:13 +0200

    Añadido Javascript SDK

---
+++ application/views/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>LogIn</title>  
+</head>
+<body>
+    <div id="fb-root"></div>
+    <a id="logIn" href="#">Log in with Facebook</a>
+    <script src="//connect.facebook.net/en_US/sdk.js"></script>
+    <script src="/assets/js/main.js"></script>
+</body>
+</html>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,38 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+
+/*
+ * Diego Castaño 2014
  */
 
+// Requires FaceBooks JS SDK
+(function (FB) {
+    
+    function logInCallback (response) {
+         if(response.status === 'connected') {
+             alert('Connected!');
+         } else {
+             alert('Not connected');
+         }
+    }
+    
+    function logIn() {
+        FB.login(logInCallback, {scope: 'user_likes'});
+    }
+    
+    /*
+     * Event handlers
+     */
+    
+    // Init FB SDK
+    document.addEventListener('DOMContentLoaded', function () {
+        FB.init({
+            appId: 1468034890110746,
+            frictionlessRequests: true,
+            status: true,
+            version: 'v2.0'
+        });
+    });
+    
+    // Handle login
+    document.querySelector('#logIn').addEventListener('click', logIn);
 
+})(FB);
