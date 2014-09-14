36f36c523fab42c2b9984bac8d18ac039f8949d2
Dowrow <dowrow@outlook.com>
2014-09-14 23:16:38 +0200

    no lib

--- application/models/MusicFactory.php
+++ application/models/MusicFactory.php
@@ -1,15 +1,34 @@
 <?php
 
+// !!
+session_start();
+
 // Composer
 require 'vendor/autoload.php';
-require 'FacebookSessionFactory.php';
+//require 'FacebookSessionFactory.php';
+
+// Namespaces
+use Facebook\FacebookSession;
+use Facebook\FacebookCanvasLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookRequestException;
+use Facebook\GraphUser;
+use Facebook\GraphPage;
+
 
 class MusicFactory {
     public static function getArtists () {
         $artists = array();
         
         try {
-            $session = FacebookSessionFactory::getSession();
+
+            // Set app keys
+            FacebookSession::setDefaultApplication('1468034890110746','09e80af7d50f86bc41d5d4895e0a978d');
+            // Get login helper
+            $helper = new FacebookCanvasLoginHelper();
+            // Get session
+            $session = $helper->getSession();
+
             if (!is_null($session)) {
 
                     $musicRequest = new FacebookRequest($session, 'GET', '/me/music');
