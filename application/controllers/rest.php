79cad16dce9faed50db6e5dc14164dbfe1ef39d8
DIEGO <dowrow@outlook.com>
2014-12-27 21:02:28 +0100

    Implementados /rest/insertartist, /rest/insertalbum, /rest/inserttag, /rest/insertfan, /rest/insertsimilar

--- application/controllers/rest.php
+++ application/controllers/rest.php
@@ -28,26 +28,95 @@ class Rest extends CI_Controller {
     // Insert an artist
     public function insertArtist () {
         
+        // Get parameters
+        $name = $this->input->get('name', TRUE);
+        $url = $this->input->get('url', TRUE);
+        $image = $this->input->get('image', TRUE);
+        
+        if ($name == FALSE || $url == FALSE || $image == FALSE) {
+            echo 'Missing arguments';
+            return;
+        }
+        
+        $this->DatabaseManager->insertArtist($name, $url, $image);
+        
+        echo 'ok';
     }
     
     // Insert an album
     public function insertAlbum () {
         
+        // Get parameters
+        $artist = $this->input->get('artist', TRUE);
+        $album = $this->input->get('album', TRUE);
+        $url = $this->input->get('url', TRUE);
+        $date = $this->input->get('date', TRUE);
+        
+        if ($artist == FALSE || $album == FALSE || $url == FALSE || $date == FALSE) {
+            echo 'Missing arguments';
+            return;
+        }
+        
+        $this->DatabaseManager->insertAlbum($artist, $album, $url, $date);
+        
+        echo 'ok';    
     }
     
     // Insert a fan
     public function insertFan () {
+       
+        // Get parameters
+        $artist = $this->input->get('artist', TRUE);
+        $album = $this->input->get('album', TRUE);
+        $url = $this->input->get('url', TRUE);
+        $date = $this->input->get('date', TRUE);
         
+        if ($artist == FALSE || $album == FALSE || $url == FALSE || $date == FALSE) {
+            echo 'Missing arguments';
+            return;
+        }
+        
+        $this->DatabaseManager->insertAlbum($artist, $album, $url, $date);
+        
+        echo 'ok';     
     }
     
     // Insert a tag
     public function insertTag () {
+        
+        // Get parameters
+        $artist = $this->input->get('artist', TRUE);
+        $name = $this->input->get('name', TRUE);
+        $url = $this->input->get('url', TRUE);
+        
+        if ($artist == FALSE || $name == FALSE || $url == FALSE) {
+            echo 'Missing arguments';
+            return;
+        }
+        
+        $this->DatabaseManager->insertTag($artist, $name, $url);
+        
+        echo 'ok';     
         
     }
     
     // Insert a similar artist
     public function insertSimilar () {
         
+        // Get parameters
+        $artist = $this->input->get('artist', TRUE);
+        $similar = $this->input->get('similar', TRUE);
+        $url = $this->input->get('url', TRUE);
+        $image =  $this->input->get('image', TRUE);
+        
+        if ($artist == FALSE || $similar == FALSE || $url == FALSE || $image == FALSE) {
+            echo 'Missing arguments';
+            return;
+        }
+        
+        $this->DatabaseManager->insertSimilar($artist, $similar, $url, $image);
+        
+        echo 'ok';     
     }
 
     
