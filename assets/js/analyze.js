326b9dfa59100db3f46192f90039970baf6e394d
Dowrow <dowrow@outlook.com>
2015-01-10 23:40:39 +0100

    test

--- assets/js/analyze.js
+++ assets/js/analyze.js
@@ -1,13 +1,32 @@
 define (['jquery'], function ($) {
     
-    function getLikes() {
-        
+     function checkLoginState() {
+        // Reset permission
+        FB.getLoginStatus(function (response) { statusChangeCallback(response); });
     }
     
+    function statusChangeCallback(response) {
+          if (response.status === 'connected') {
+              location.href='/analyze';
+          } else if (response.status === 'not_authorized') {
+             FB.login(function(response) { statusChangeCallback(response); }, {scope: 'user_likes'});
+          } else {
+             alert("Please login.");
+          }
+    }
+    
+    // Load async. FB SDK
+    (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&appId=1468034890110746&version=v2.0";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
     
     // DOM callbacks
     $(document).ready(function () {
-        
+        checkLoginState();
         // Animate progress bar
         $('#appProgress').css('width', '33%');
     
