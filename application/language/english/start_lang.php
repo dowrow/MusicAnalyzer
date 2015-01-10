8a1aaccf35c499244b29feb17de0fe2d0a2f09b5
Dowrow <dowrow@outlook.com>
2015-01-10 07:00:25 +0100

    bootstrap start

---
+++ application/language/english/start_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+// Strings used in start.php view
+$lang['start_title'] = "Music Analyzer";
+$lang['start_description'] = "Discover what your Facebook music says about you.";
+$lang['start_button'] = "Start";
--- application/views/start.php
+++ application/views/start.php
@@ -15,16 +15,30 @@
         
         <div class="container">
             <div class="jumbotron text-center">
-                <h1 >Music Analyzer</h1>
-                <p class="lead">Discover what your Facebook Music says about you.</p>
-                <p><a class="btn btn-primary btn-lg" href="javascript:void(0);" role="button">Start</a></p>
+                <h1><?php echo $this->lang->line('start_title'); ?></h1>
+                <p class="lead"><?php echo $this->lang->line('start_description'); ?></p>
+                <p><a class="btn btn-primary btn-lg" href="javascript:void(0);" role="button"><?php echo $this->lang->line('start_button'); ?></a></p>
             </div>
-         </div>
+            <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+        
+        </div>
+        
+        
+        <!-- FB root -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&appId=1468034890110746&version=v2.0";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+        
     </body>
     
 </html>
