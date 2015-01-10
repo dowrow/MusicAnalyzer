f09c6d235f1ab9ba012c6899c99e4eafd18aa77d
Dowrow <dowrow@outlook.com>
2015-01-10 23:57:03 +0100

    async init scope

--- assets/js/analyze.js
+++ assets/js/analyze.js
@@ -1,21 +1,21 @@
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1468034890110746',
-      xfbml      : true,
-      version    : 'v2.1'
-    });
-  };
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
 
 define (['jquery'], function ($) {
-    
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '1468034890110746',
+          xfbml      : true,
+          version    : 'v2.1'
+        });
+      };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/all.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+
      function checkLoginState() {
         // Reset permission
         FB.getLoginStatus(function (response) { statusChangeCallback(response); });
