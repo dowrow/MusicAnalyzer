a0033057915466401ba60765336349f4ba3d6aac
Dowrow <dowrow@outlook.com>
2015-01-10 08:08:51 +0100

    FB login 2

--- assets/js/start.js
+++ assets/js/start.js
@@ -7,9 +7,8 @@ define (['jquery'], function ($) {
     
     function statusChangeCallback(response) {
           if (response.status === 'connected') {
-              console.log('Conectado');
+              location.href='/analyze';
           } else if (response.status === 'not_authorized') {
-             console.log('En FB pero sin login en la app');
              FB.login(function(response) { statusChangeCallback(response); }, {scope: 'user_likes'});
           } else {
              alert("Please login.");
