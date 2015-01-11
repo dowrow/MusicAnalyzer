dd15ee67f16f8187c4bd258e6d6ea65981372416
Dowrow <dowrow@outlook.com>
2015-01-11 16:49:59 +0100

    test /me/music 4

--- assets/js/analyze.js
+++ assets/js/analyze.js
@@ -1,36 +1,36 @@
 
 define (['jquery', 'facebook'], function ($, facebook) {
-     
-    FB.init({
-        appId      : '1468034890110746',
-        xfbml      : true,
-        status     : true,
-        cookie     : true,
-        version    : 'v2.1'
-    });
-     /*
-    function checkLoginState() {
-        // Reset permission
-        FB.getLoginStatus(function (response) { statusChangeCallback(response); });
+    
+    function getUserMusic(callback) {
+        FB.init({
+            appId      : '1468034890110746',
+            xfbml      : true,
+            status     : true,
+            cookie     : true,
+            version    : 'v2.1'
+        });
+
+        FB.login(function () {
+            FB.api(
+                "/me/music",
+                function (response) {
+                  if (!response.data) {
+                      console.log('Error getting user music');
+                  } else {
+                      callback(response.data)
+                  }       
+            });
+        }, {scope: 'user_likes'});
     }
     
-    function statusChangeCallback(response) {
-        
-          if (response.status === 'connected') {
-              console.log('ok');
-          } else if (response.status === 'not_authorized') {
-             FB.login(function(response) { statusChangeCallback(response); }, {scope: 'user_likes'});
-          } else {
-             alert("Please login.");
-          }
-    }
-    */
+    // Get user music ASAP
+    getUserMusic(function (artists) {
+        console.log(artists);
+    });
 
     // DOM callbacks
     $(document).ready(function () {
         
-        //setTimeout(checkLoginState, 1000);
-    
         // Animate progress bar
         $('#appProgress').css('width', '33%');
     
@@ -51,15 +51,6 @@ define (['jquery', 'facebook'], function ($, facebook) {
                     break;
             }
         }, 500);
-        
-        FB.login(function () {
-            FB.api(
-                "/me/music",
-                function (response) {
-                  console.log('me/music:');         
-                  console.log(response);              
-            });
-        }, {scope: 'user_likes'});
         
     });
 });
