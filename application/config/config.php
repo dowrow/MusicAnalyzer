051f0df732d8a4bde5020bcc8a03bdbbf5a78ea7
Dowrow <dowrow@outlook.com>
2015-02-07 19:42:46 +0100

    Arreglar sesiones en IE

--- application/config/config.php
+++ application/config/config.php
@@ -244,12 +244,12 @@ $config['encryption_key'] = '1234';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
+$config['sess_cookie_name']		= 'cisession';
 $config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
+$config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
-$config['sess_table_name']		= 'ci_sessions';
+$config['sess_table_name']		= 'cisessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update']	= 300;
