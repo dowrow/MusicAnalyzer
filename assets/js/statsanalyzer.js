b9ed58d198b7cab12c888c364df9b2b4ff1da465
Dowrow <dowrow@outlook.com>
2015-03-02 21:50:49 +0100

    test results 1k

--- assets/js/statsanalyzer.js
+++ assets/js/statsanalyzer.js
@@ -13,10 +13,13 @@ define (['jquery', 'aggregate'], function ($, aggregate) {
         var part2 = $('#result-age-2').val();
         var age = 0;
         var count = 0;
-        console.log(stats);
+        
         // Average fan age
         for (var artist in stats) {
             if (stats.hasOwnProperty(artist)) {
+                console.log("stats de " + artist);
+                console.log(stats[artist]);
+
                 if (stats[artist].averageFanAge !== -1) {
                     console.log('fan age ' + stats[artist].averageFanAge);
                     age += parseInt(stats[artist].averageFanAge); 
