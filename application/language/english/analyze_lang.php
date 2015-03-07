ac441c5396cb0cf6f39b629a4c283a0698487fc8
Dowrow <dowrow@outlook.com>
2015-03-07 20:51:41 +0100

    English result text

--- application/language/english/analyze_lang.php
+++ application/language/english/analyze_lang.php
@@ -2,10 +2,20 @@
 
 // EN
 $lang['analyze_title'] = "Analyzing your likes";
+
 $lang['analyze_status'] = "Collecting info about";
 $lang['analyze_status_album'] = "Searching albums by";
 $lang['analyze_status_fan'] = "Searching fans of";
 $lang['analyze_status_tag'] = "Determining the style of";
 $lang['analyze_status_similar'] = "Searching artists similar to";
+
 $lang['analyze_error_title'] = "Looks like there's a problem";
 $lang['analyze_error_status'] = "We were unable to find music among your likes.";
+
+$lang['analyze_result_age_1'] = "You listen to the same music as an ";
+$lang['analyze_result_age_2'] = " years old person.";
+$lang['analyze_result_style'] = "Your favorite styles are: ";
+$lang['analyze_result_epoch_1'] = "You wish you were born on ";
+$lang['analyze_result_epoch_2'] = " when ";
+$lang['analyze_result_epoch_3'] = " released the album ";
+$lang['analyze_result_similar'] = "You will surely like this too: ";
