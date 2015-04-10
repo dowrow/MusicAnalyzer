1ba6170d1252702473b366239615ef6e9e88e2c8
Dowrow <dowrow@outlook.com>
2015-04-10 23:14:20 +0200

    highchart 12 top title

--- application/views/start.php
+++ application/views/start.php
@@ -89,10 +89,10 @@
             
             <!-- Results template -->
             <div class="hidden" id="results">
-                
                 <h1 class="text-center">
-                    <?php echo $this->lang->line('analyze_results_title'); ?>
+                    <?php echo $this->lang->line('analyze_friend_results_title'); ?>
                 </h1>
+                
                 <br/>
                 <div class="jumbotron">
                     
@@ -102,6 +102,10 @@
                         <button class="btn btn-primary btn-lg text-right" id="invite">Invite Friends</button>
                     </span>
                 </div>
+                <h1 class="text-center">
+                    <?php echo $this->lang->line('analyze_results_title'); ?>
+                </h1>
+                <br/>
                  <div class="jumbotron">
                     <span class="icon"><span class="glyphicon glyphicon-user"></span></span>
                     <p id="age_result"></p>
@@ -138,10 +142,8 @@
                      </span>
                 </div>
                 
-                <!--<h1 class="text-center">
-                    <?php echo $this->lang->line('analyze_friend_results_title'); ?>
-                </h1>
-                -->
+                
+               
                 
             </div>            
             <!-- Like button -->
